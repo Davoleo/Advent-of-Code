88038ff5f25ac4a2dc95f8a4eb903ca46f1b3412
Davoleo <dav@davoleo.net>
2022-12-05 09:49:56 +0100

    Day 04 [complete]

--- 2022/day04/Day04.kt
+++ 2022/day04/Day04.kt
@@ -2,16 +2,13 @@ package day04
 
 import java.io.File
 
-fun doRangesOverlap(range1: IntRange, range2: IntRange): Boolean {
-    return range1.first <= range2.last && range2.first >= range1.last
-}
-
 fun main() {
 
     println("### Day 04: Camp Cleanup ###")
     println("--- Part 1 ---")
 
     var subsetSections = 0
+    var overlappingSections = 0
 
     File("2022\\day04\\input.txt").forEachLine { line ->
 
@@ -32,27 +29,17 @@ fun main() {
         if (fcs || scf) {
             subsetSections++
         }
+
+        if (pair.first.intersect(pair.second).isNotEmpty()) {
+            overlappingSections++
+        }
     }
 
     println("Number of section ranges fully contained by other section range of the pair: $subsetSections")
 
     println("--- Part 2 ---")
 
-    val lines = File("2022\\day04\\input.txt").readLines()
-
-    val decoupledRanges: List<IntRange> = List(lines.size * 2) { index ->
-        val rangeString = lines[index/2].split(",")[index%2]
-        val rangeLimits = rangeString.split("-")
-        IntRange(rangeLimits[0].toInt(), rangeLimits[1].toInt())
-    }
-
-    var overlapsCount = 0
-    decoupledRanges.forEach { range1 ->
-        if (decoupledRanges.any { doRangesOverlap(range1, it) })
-            overlapsCount++
-    }
-
-    println("Ranges overlapping overall: $overlapsCount")
+    println("Ranges overlapping overall: $overlappingSections")
 
 
 }
