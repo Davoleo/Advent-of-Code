b9cdfdae3110d715d32893dabc075a85555f0ad5
Davoleo <dav@davoleo.net>
2022-12-13 19:01:34 +0100

    day08: Fix Part 1 + add part 2

--- 2022/day08/day08.kt
+++ 2022/day08/day08.kt
@@ -4,9 +4,75 @@ import java.io.File
 
 const val MATRIX_SIZE = 99
 
-private fun BooleanArray.set(indices: IntRange, value: Boolean) {
-    for (i in indices)
-        this[i] = value
+val visibilityCache = BooleanArray(MATRIX_SIZE* MATRIX_SIZE)
+val matrix = Array(MATRIX_SIZE) {_ -> IntArray(MATRIX_SIZE) }
+
+fun testTreeVisibility(row: Int, col: Int, num: Int, horizontalMaxCache: IntArray, verticalMaxCache: IntArray) {
+    var overMax = false
+
+    //Test Horizontal Visibility
+    if (horizontalMaxCache[row] < num) {
+        horizontalMaxCache[row] = num
+        overMax = true
+    }
+
+    //Test Top Visibility
+    if (verticalMaxCache[col] < num) {
+        verticalMaxCache[col] = num
+        overMax = true
+    }
+    if (overMax)
+        visibilityCache[row*MATRIX_SIZE + col] = true
+}
+
+fun treeScenicScore(row: Int, col: Int, tree: Int): Int {
+
+    var scenicScore = 1
+
+    //Left Scenic Score
+    var x = col
+    var treeCount = 0
+    while (x > 0) {
+        x--
+        treeCount++
+        if (matrix[row][x] >= tree)
+            break
+    }
+    scenicScore *= treeCount
+
+    //Right Scenic Score
+    x = col
+    treeCount = 0
+    while (x < MATRIX_SIZE-1) {
+        x++
+        treeCount++
+        if (matrix[row][x] >= tree)
+            break
+    }
+    scenicScore *= treeCount
+
+
+    var y = row
+    treeCount = 0
+    while (y > 0) {
+        y--
+        treeCount++
+        if (matrix[y][col] >= tree)
+            break
+    }
+    scenicScore *= treeCount
+
+    y = row
+    treeCount = 0
+    while (y < MATRIX_SIZE-1) {
+        y++
+        treeCount++
+        if (matrix[y][col] >= tree)
+            break
+    }
+    scenicScore *= treeCount
+
+    return scenicScore
 }
 
 fun main() {
@@ -17,57 +83,49 @@ fun main() {
     val lines = File("2022\\day08\\input.txt").readLines()
     //All the trees at the sides of the matrix are always visible
 
-    val leftHighest = IntArray(MATRIX_SIZE) { _ -> 0}
-    val rightHighest = IntArray(MATRIX_SIZE) { _ -> 0}
+    lines.forEachIndexed { row, line -> line.forEachIndexed { col, char -> matrix[row][col] = char.code - '0'.code } }
 
-    val topHighest = IntArray(MATRIX_SIZE) { _ -> 0}
-    val bottomVisible = Array(MATRIX_SIZE) { _ -> BooleanArray(10) }
+    val leftHighest = IntArray(MATRIX_SIZE) { i -> lines[i][0].code - '0'.code }
+    val topHighest = IntArray(MATRIX_SIZE) { i -> lines[0][i].code - '0'.code }
+
+    val rightHighest = IntArray(MATRIX_SIZE) { i ->
+        val line = lines[i]
+        line[line.lastIndex].code - '0'.code
+    }
+    val bottomHighest = IntArray(MATRIX_SIZE) { i -> lines[lines.lastIndex][i].code - '0'.code }
 
     var visibleTrees = MATRIX_SIZE*4 - 4
 
     println("--- Part 1 ---")
 
-    lines.forEachIndexed { row, line ->
+    for (row in 1 until MATRIX_SIZE-1) {
+        for (col in 1 until MATRIX_SIZE-1) {
+            val num = lines[row][col].code - '0'.code
+            testTreeVisibility(row, col, num, leftHighest, topHighest)
 
-        if (row in 1 until MATRIX_SIZE-1) {
-            for (col in 1 until line.length-1) {
-
-                //Test Left Visibility
-                val numLeft = line[col].code - '0'.code
-                if (leftHighest[row] < numLeft) {
-                    leftHighest[row] = numLeft
-                    ++visibleTrees
-                    continue
-                }
-
-                //Test Top Visibility
-                if (topHighest[col] < numLeft) {
-                    topHighest[col] = numLeft
-                    ++visibleTrees
-                    continue
-                }
-
-                //Test Right Visibility
-                val numRight = line[line.length - col].code - '0'.code
-                if (rightHighest[row] < numRight) {
-                    rightHighest[row] = numRight
-                    ++visibleTrees
-                    continue
-                }
-
-                val flags = bottomVisible[col]
-                if (!flags[numLeft]) {
-                    flags[numLeft] = true
-                    flags.set(0 until numLeft, false)
-                }
-
-            }
+            val rrow = MATRIX_SIZE-1-row
+            val rcol = MATRIX_SIZE-1-col
+            val rnum = lines[rrow][rcol].code - '0'.code
+            testTreeVisibility(rrow, rcol, rnum, rightHighest, bottomHighest)
         }
-
     }
 
-    for (treeCol in bottomVisible)
-        visibleTrees += treeCol.count { it }
-
+    visibleTrees += visibilityCache.count { it }
     println("Number of visible trees: $visibleTrees")
+
+    println("--- Part 2 ---")
+
+    var maxScenicScore = 0
+    for (row in 0 until MATRIX_SIZE) {
+        for (col in 0 until MATRIX_SIZE) {
+
+            val tree = matrix[row][col]
+            val scenicScore = treeScenicScore(row, col, tree)
+            if (scenicScore > maxScenicScore)
+                maxScenicScore = scenicScore
+        }
+    }
+
+
+    println("Highest Scenic Score Tree: $maxScenicScore")
 }
