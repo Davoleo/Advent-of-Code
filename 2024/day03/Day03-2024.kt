38f6025d3f0e1ce9cb7deaec514bee13b81470a7
davoleo <dav@davoleo.net>
2024-12-03 13:23:50 +0100

    Day03-2024 (part2)

--- 2024/day03/Day03-2024.kt
+++ 2024/day03/Day03-2024.kt
@@ -20,7 +20,33 @@ fun main() {
         sum += (factors[0] * factors[1])
     }
 
-    println("The sum of all products in the computer's code is: $sum")
-    //println("--- Part 2 ---")
+    println("The sum of all products in the code is: $sum")
+    println("--- Part 2 ---")
+
+    val regex2 = Regex("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)")
+    var conditionedSum = 0
+    var enabled = true
+
+    for (matchResult in regex2.findAll(code)) {
+        val match = matchResult.value
+        if (match == "do()") {
+            enabled = true
+            continue
+        }
+        if (match == "don't()") {
+            enabled = false
+            continue
+        }
+
+        if (enabled) {
+            val factors = match
+                .slice(4 until match.length-1)
+                .split(",")
+                .map(String::toInt)
+            conditionedSum += factors[0] * factors[1]
+        }
+    }
+
+    println("The sum of all products in the code with conditions is: $conditionedSum")
 
 }
