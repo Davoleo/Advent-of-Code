b378ad92373ff89a4ac130b4a19f14f25639eb4e
davoleo <dav@davoleo.net>
2024-12-04 13:41:05 +0100

    Day04 - part 1 (incomplete)

---
+++ 2024/day04/Day04-2024.kt
@@ -0,0 +1,106 @@
+package day04
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+val text = Files.readString(Path.of("2024", "day04", "test.txt"))
+val width = text.indexOf('\n')
+val height = text.length / (width+1)
+
+const val pattern = "XMAS"
+
+fun main() {
+
+    println("--- Part 1 ---")
+    println("matrix: $width x $height")
+
+    var occurrences = 0
+
+    for (i in 0 until height) {
+        for (j in 0 until width) {
+            val index = i*width+j
+            if (text[index] == 'X') {
+                occurrences += starPatternMatching(index)
+            }
+        }
+    }
+
+    println("The number of XMAS occurrences in the text is: $occurrences")
+    //1922 is too low
+}
+
+fun starPatternMatching(index: Int): Int {
+    var occurrences = 0
+
+    occurrences += cardinalPatternMatching(index, CardinalAxis.VERTICAL)
+
+    occurrences += cardinalPatternMatching(index, CardinalAxis.HORIZONTAL)
+
+    occurrences += diagonalPatternMatching(index, DiagonalAxis.DESCENDING)
+
+    //TODO : broken ascending logic
+    //occurrences += diagonalPatternMatching(index, DiagonalAxis.ASCENDING)
+
+    return occurrences
+}
+
+enum class CardinalAxis {
+    VERTICAL,
+    HORIZONTAL
+}
+
+fun cardinalPatternMatching(index: Int, type: CardinalAxis): Int {
+    var forward = true
+    var reverse = true
+    for (i in 1..3) {
+        val offset = if (type == CardinalAxis.VERTICAL) i*width else i
+
+        // Out of bounds
+        if (index+offset >= text.length) forward = false
+        if (index-offset < 0) reverse = false
+
+        if (forward && text[index+offset] != pattern[i]) {
+            forward = false
+        }
+
+        if (reverse && text[index-offset] != pattern[i]) {
+            reverse = false
+        }
+    }
+    return when {
+        forward.and(reverse) -> 2
+        forward.xor(reverse) -> 1
+        else -> 0
+    }
+}
+
+enum class DiagonalAxis {
+    DESCENDING,
+    ASCENDING
+}
+
+fun diagonalPatternMatching(index: Int, type: DiagonalAxis): Int {
+    var forward = true
+    var reverse = true
+    for (i in 1..3) {
+        val offset = if (type == DiagonalAxis.DESCENDING) i*width+i else i*width-i
+
+        // Out of bounds
+        if (index+offset >= text.length) forward = false
+        if (index-offset < 0) reverse = false
+
+
+        if (forward && text[index+offset] != pattern[i]) {
+            forward = false
+        }
+
+        if (reverse && text[index-offset] != pattern[i]) {
+            reverse = false
+        }
+    }
+    return when {
+        forward.and(reverse) -> 2
+        forward.xor(reverse) -> 1
+        else -> 0
+    }
+}
