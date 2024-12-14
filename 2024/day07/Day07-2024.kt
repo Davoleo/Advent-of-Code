6663487da8153aaf44a490ea04399d6f62f1e6bd
Davoleo <dav@davoleo.net>
2024-12-14 19:08:39 +0100

    Day07 - part 2

--- 2024/day07/Day07-2024.kt
+++ 2024/day07/Day07-2024.kt
@@ -4,13 +4,20 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.util.function.BinaryOperator
 
-enum class Operator(val symbol: Char, private val func: BinaryOperator<Long>) {
-    ADD('+', Long::plus),
-    MULTIPLY('*', Long::times),
+enum class Operator(val symbol: String, private val func: BinaryOperator<Long>) {
+    ADD("+", Long::plus),
+    MULTIPLY("*", Long::times),
+    CONCATENATE("||", { a, b -> (a.toString() + b.toString()).toLong() })
     ;
 
     operator fun invoke(a: Long, b: Long): Long {
         return this.func.apply(a, b)
+    }
+
+    companion object {
+        fun legalOps(): Array<Operator> {
+            return arrayOf(ADD, MULTIPLY)
+        }
     }
 }
 
@@ -22,28 +29,36 @@ fun main() {
 
     val input = Files.readAllLines(Path.of("2024", "day07", "input.txt"))
 
-    var trueAcc: Long = 0
+    var validResultsPart1: Long = 0
+    var validResultsPart2: Long = 0
     for (equation in input) {
         val (result, inputs) = equation.split(':')
         val operands = inputs.trim().split(' ').map(String::toInt)
 
         val resultLong = result.toLong()
 
-        val valid = isEqValid(resultLong, operands.first().toLong(), 1, operands)
-        if (valid) {
-            trueAcc += resultLong
-        }
+        val valid1 = isEqValid(resultLong, operands.first().toLong(), 1, operands, Operator.legalOps())
+        if (valid1)
+            validResultsPart1 += resultLong
+
+        val valid2 = isEqValid(resultLong, operands.first().toLong(), 1, operands, Operator.entries.toTypedArray())
+        if (valid2)
+            validResultsPart2 += resultLong
     }
 
-    println("The total calibration result is $trueAcc")
+    println("The total calibration result is $validResultsPart1")
+
+    println("--- Part 2 ---")
+
+    println("The total calibration result is $validResultsPart2")
 }
 
-fun isEqValid(testValue: Long, accumulator: Long, i: Int, operands: List<Int>): Boolean {
+fun isEqValid(testValue: Long, accumulator: Long, i: Int, operands: List<Int>, operators: Array<Operator>): Boolean {
     if (i < operands.size) {
         var isValid = false
-        for (op in Operator.entries) {
+        for (op in operators) {
             val newVal = op(accumulator, operands[i].toLong())
-            isValid = isValid || isEqValid(testValue, newVal, i+1, operands)
+            isValid = isValid || isEqValid(testValue, newVal, i+1, operands, operators)
         }
         return isValid
     }
